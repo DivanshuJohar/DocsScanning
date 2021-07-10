f93c56fa8f400b0aeb6843ff76c03d31d7ef2c75
divan <macdivanshu@gmail.com>
2021-07-10 14:12:22 +0530

    Divanshu Check 10

---
+++ app/src/main/java/com/example/docsscanning/Share_Activity.java
@@ -0,0 +1,55 @@
+package com.example.docsscanning;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Share_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        // first parameter is the file for icon and second one is menu
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // We are using switch case because multiple icons can be kept
+        switch (item.getItemId()) {
+            case R.id.shareButton:
+
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+
+                // type of the content to be shared
+                sharingIntent.setType("text/plain");
+
+                // Body of the content
+                String shareBody = "Your Body Here";
+
+                // subject of the content. you can share anything
+                String shareSubject = "Your Subject Here";
+
+                // passing body of the content
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+
+                // passing subject of the content
+                sharingIntent.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
+                startActivity(Intent.createChooser(sharingIntent, "Share using"));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
