3f9ff11ad7046ace447cfb93a4fb41b15114c3ca
divan <macdivanshu@gmail.com>
2021-07-30 10:54:36 +0530

    Divanshu Check 42

--- app/src/main/java/com/example/docsscanning/Dashboard.java
+++ app/src/main/java/com/example/docsscanning/Dashboard.java
@@ -16,18 +16,33 @@ import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+
 public class Dashboard extends AppCompatActivity {
     private ActionBar toolbar;
     BottomNavigationView bottomNavigationView;
+    SearchView searchView;
+    ListView myList;
+    ArrayList<String> list ;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+         searchView = findViewById(R.id.searchView);
+         myList = findViewById(R.id.searchlistview);
+         list.add("");
+         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+         myList.setAdapter(adapter);
+
         BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
         bottomNav.setSelectedItemId(R.id.doclist);
@@ -67,9 +82,12 @@ public class Dashboard extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
+        Fragment fragment = null;
         switch (item.getItemId()) {
             case R.id.search:
                 // do your code
+                fragment = new GalleryFragment();
+                fragmentmanager(fragment);
                 return true;
             case R.id.merge:
                 // do your code
